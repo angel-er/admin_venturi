3027f907798a723fa6fc68389eafbc4e9cb56559
angel-er <a.encinasrm@gmail.com>
2025-01-30 18:51:05 -0400

    construccion del navbar y siebar con material ui

---
+++ src/containers/pages/Clients.js
@@ -0,0 +1,9 @@
+function Clients(params) {
+  return (
+    <div>
+      <h1>WELCOME CLIENTS</h1>
+    </div>
+  );
+}
+
+export default Clients;
---
+++ src/containers/pages/Store.js
@@ -0,0 +1,9 @@
+function Store(params) {
+  return (
+    <div>
+      <h1>WELCOME STORE</h1>
+    </div>
+  );
+}
+
+export default Store;
