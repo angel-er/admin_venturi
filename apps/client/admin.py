7e92b5f31a9d722f20d0a900e1fa6f91d45e14c9
angel-er <a.encinasrm@gmail.com>
2025-02-17 15:41:50 -0400

    Model Product created

--- apps/client/admin.py
+++ apps/client/admin.py
@@ -3,7 +3,7 @@ from .models import *
 
 # Register your models here.
 class ClientAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'last_name', 'telephone', 'email',)
+    list_display = ('id', 'name', 'last_name', 'telephone', 'email', 'created', 'updated',)
     list_display_links = ('name', 'last_name', 'email',)
     list_per_page = 25
 
---
+++ apps/product/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name_product', 'description', 'price_product', 'image', 'price_product', 'created', 'updated',)
+    list_display_links = ('name_product', 'description', 'price_product', 'created', 'updated',)
+    list_per_page = 25
+
+admin.site.register(Product, ProductAdmin)
