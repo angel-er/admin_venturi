2e8b29c7ec4c1526bcc566854cc066b2b8a5506f
angel-er <a.encinasrm@gmail.com>
2025-02-25 23:59:22 -0400

    The component LineChart and Bar chart was added in the container Dashboard

--- src/containers/pages/Dashboard/Dashboard.js
+++ src/containers/pages/Dashboard/Dashboard.js
@@ -1,7 +1,13 @@
 import { Box, Divider, Typography } from "@mui/material";
 import Grid from "@mui/material/Grid2";
 
+import { LineGraph } from "#components/ChartLine.js";
+import { barChartData, lineChartData } from "./FAKE_DATA";
+import { BarGraph } from "#components/ChartBar.js";
+import theme from "#config/theme.js";
+
 function DashboardContainer() {
+  const options = {};
   return (
     <Box>
       <Typography sx={styles.pageTitle} variant="h5">
@@ -9,14 +15,33 @@ function DashboardContainer() {
       </Typography>
 
       <Divider style={styles.divider} />
-      <Grid container spacing={1}>
-        <Grid size={6}></Grid>
-        <Grid size={6}></Grid>
+      <Grid container spacing={10}>
+        <Grid size={{ xs: 12, md: 6 }}>
+          <Typography sx={{ textAlign: "center" }}>VENTAS DIARIAS</Typography>
+          <LineGraph options={options} data={lineChartData} />
+        </Grid>
+        <Grid size={{ xs: 12, md: 6 }}>
+          <Typography sx={{ textAlign: "center" }}>
+            VENTAS POR SEMANA
+          </Typography>
+          <BarGraph options={options} data={barChartData} />
+        </Grid>
+        <Grid size={{ xs: 12, md: 6 }}>
+          <Typography sx={{ textAlign: "center" }}>
+            PRODUCTO MAS VENDIDO
+          </Typography>
+          <BarGraph options={options} data={barChartData} />
+        </Grid>
       </Grid>
     </Box>
   );
 }
 
-const styles = {};
+const styles = {
+  divider: { mb: 3, borderColor: theme.palette.grey.main, marginBottom: 20 },
+  pageTitle: {
+    mb: 5,
+  },
+};
 
 export default DashboardContainer;
